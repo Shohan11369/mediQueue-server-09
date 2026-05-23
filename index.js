b2e49e4fbadeaa4d910c217b23913142f0ef30f5
Shohan111369 <sarkershohan346@gmail.com>
2026-05-24 02:34:49 +0800

    again added login system

--- index.js
+++ index.js
@@ -119,6 +119,23 @@ async function run() {
       res.send(result);
     });
 
+    // login 
+    app.post("/auth/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersCollection.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({ message: "User not found" });
+  }
+
+  if (user.password !== password) {
+    return res.status(401).json({ message: "Wrong password" });
+  }
+
+  res.json({ message: "Login successful", user });
+});
+
     // register auth check
     app.post("/auth/register", async (req, res) => {
       const { name, email, password, image } = req.body;
